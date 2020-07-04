fee2dda29b8cf782412aedf9a7e43ba07bae7d8e
“Евгений <sishnenko86@mail.ru>
2020-07-04 21:51:33 +0300

    переместил useEffect в компонент list

--- src/App.js
+++ src/App.js
@@ -4,28 +4,10 @@ import Details from "./Components/Users/Details";
 import "./App.css";
 
 function App() {
-  const [list, setList] = useState();
   const [info, setInfo] = useState();
-  const [isLoading, setIsLoading] = useState(false);
   const [isLoadingDetails, setIsLoadingDetails] = useState(false);
-
   const infoRef = useRef();
-  useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json"
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        setList(result);
-        setIsLoading(true);
-      })
-      .catch((e) => console.log(e))
-      .finally(function () {
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 500);
-      });
-  }, []);
+
   function showDetails(id) {
     if (info) {
       infoRef.current = info.id;
@@ -53,8 +35,8 @@ function App() {
         <div className="row mt-4">
           <Lists
             onShowDetails={showDetails}
-            isLoading={isLoading}
-            list={list}
+            // isLoading={isLoading}
+            // list={list}
           />
           {info && <Details info={info} isLoadingDetails={isLoadingDetails} />}
         </div>
--- src/Components/Users/List.js
+++ src/Components/Users/List.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 function Lists(props) {
-  const { list, isLoading } = props;
+  const [list, setList] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // const { list, isLoading } = props;
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json"
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setList(result);
+        setIsLoading(true);
+      })
+      .catch((e) => console.log(e))
+      .finally(function () {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+      });
+  }, []);
   function onShowDetails(e) {
     props.onShowDetails(e.target.dataset.id);
     const wrapObj = document.querySelector(".list-group ");
